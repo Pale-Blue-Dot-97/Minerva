8cbc3abec04eaf3423f03963ccf6303a246a8330
Harry Baker <hjbaker97@gmail.com>
2021-01-07 15:00:58 +0000

    API Key Request Added

---
+++ Exp1.py
@@ -0,0 +1,7 @@
+import requests
+
+# copy your API key from dashboard.mlhub.earth and paste it in the following
+API_KEY = 'PASTE_YOUR_API_KEY_HERE'
+API_BASE = 'https://api.radiant.earth/mlhub/v1'
+
+COLLECTION_ID = 'ref_landcovernet_v1_labels'
