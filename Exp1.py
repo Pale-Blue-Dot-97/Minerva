c702b2a4c595599d4d8f88aeab0c454bd2c17bc4
Harry Baker <hjbaker97@gmail.com>
2021-01-07 15:34:17 +0000

    Corrected filename

--- Exp1.py
+++ Exp1.py
@@ -1,7 +1,7 @@
 import requests
 
 # copy your API key from dashboard.mlhub.earth and paste it in the following
-API_KEY = open('API Key.txt', 'r').read()
+API_KEY = open('API Key', 'r').read()
 API_BASE = 'https://api.radiant.earth/mlhub/v1'
 
 COLLECTION_ID = 'ref_landcovernet_v1_labels'
