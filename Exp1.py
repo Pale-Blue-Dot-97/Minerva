d4dbb8b0df2e3cc070a9fb9e6cd3e970341656f9
Harry Baker <hjbaker97@gmail.com>
2021-01-07 15:03:23 +0000

    Finding Possible Land Cover Labels

--- Exp1.py
+++ Exp1.py
@@ -11,3 +11,11 @@ print(f'Description: {r.json()["description"]}')
 print(f'License: {r.json()["license"]}')
 print(f'DOI: {r.json()["sci:doi"]}')
 print(f'Citation: {r.json()["sci:citation"]}')
+
+r = requests.get(f'{API_BASE}/collections/{COLLECTION_ID}/items', params={'key': API_KEY})
+label_classes = r.json()['features'][0]['properties']['label:classes']
+for label_class in label_classes:
+    print(f'Classes for {label_class["name"]}')
+    for c in sorted(label_class['classes']):
+        print(f'- {c}')
+
