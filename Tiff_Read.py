2dd6d30165b373caef94c8b1c6f786db7bb2a4c5
Harry Baker <hjbaker97@gmail.com>
2021-01-11 11:12:17 +0000

    Tiff_Read preamble

---
+++ Tiff_Read.py
@@ -0,0 +1,14 @@
+"""Tiff_Read
+
+Script to locate, open, read and manipulate .tiff images and datasets downloaded from the Radiant MLHub API using
+rasterio.
+
+TODO:
+    *
+
+"""
+# =====================================================================================================================
+#                                                     IMPORTS
+# =====================================================================================================================
+import os
+import rasterio
