5e53e8c7f68a81bc75eade3c16111ae1f6547db8
Harry Baker <hjbaker97@gmail.com>
2021-01-18 23:17:24 +0000

    Completed TODO

--- Tiff_Read.py
+++ Tiff_Read.py
@@ -4,7 +4,6 @@ Script to locate, open, read and manipulate .tiff images and datasets downloaded
 rasterio.
 
 TODO:
-    * Generalise to plot heatmaps of all label masks
     * Fully document
 
 """
