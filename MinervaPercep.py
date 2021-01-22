1d4f65afc8011b9dfa882b194b027187f1179638
Harry Baker <hjbaker97@gmail.com>
2021-01-22 14:13:59 +0000

    Name change. Added TODOs

---
+++ MinervaPercep.py
@@ -0,0 +1,12 @@
+"""MinervaPercep
+
+Script to create a simple MLP to classify land cover of the images in the LandCoverNet V1 dataset
+
+TODO:
+    * Add methods to locate, load, pre-process and arrange data and labels from LandCoverNet
+    * Add methods to split the data into train and test
+    * Construct a simple MLP
+    * Add methods to visualise and analyse results of training and testing
+    * Add method to save models to file
+
+"""
