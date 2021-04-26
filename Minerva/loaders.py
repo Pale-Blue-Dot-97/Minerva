4da685e109a717f4a2e976a71415a999a5a1fcb8
Harry Baker <hjbaker97@gmail.com>
2021-04-26 12:28:40 +0100

    Added License header to source files

--- Minerva/loaders.py
+++ Minerva/loaders.py
@@ -2,6 +2,28 @@
 
 Module containing classes defining custom dataloaders for use in the fitting of neural networks
 
+    Copyright (C) 2021 Harry James Baker
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program in LICENSE.txt. If not,
+    see <https://www.gnu.org/licenses/>.
+
+Author: Harry James Baker
+Email: hjb1d20@soton.ac.uk or hjbaker97@gmail.com
+Institution: University of Southampton
+
+Created under a project funded by the Ordnance Survey Ltd
+
 TODO:
     * Fully document
     * Add Loader for whole images
--- Minerva/utils/DataVis.py
+++ Minerva/utils/DataVis.py
@@ -3,7 +3,27 @@
 Script to locate, open, read and visualise .tiff images and associated label masks downloaded from the
 Radiant MLHub API.
 
-Author: Harry Baker
+    Copyright (C) 2021 Harry James Baker
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program in LICENSE.txt. If not,
+    see <https://www.gnu.org/licenses/>.
+
+Author: Harry James Baker
+Email: hjb1d20@soton.ac.uk or hjbaker97@gmail.com
+Institution: University of Southampton
+
+Created under a project funded by the Ordnance Survey Ltd
 
 
 TODO:
