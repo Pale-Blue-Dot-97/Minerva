e06b07c9cf3a389f8a18a6afb7c17405abdac990
Harry Baker <hjbaker97@gmail.com>
2021-04-26 14:27:33 +0100

    Added README import to setup.py

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Minerva
+
+Package to aid in the building, fitting and testing of neural network models on land cover data.
  
--- setup.py
+++ setup.py
@@ -1,8 +1,16 @@
 from setuptools import setup
+from os import path
+
+# Read contents of README file
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(name='Minerva',
       version='0.1',
       description='Package to build, train and test neural network models on land cover data',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Harry James Baker',
       author_email='hjb1d20@soton.ac.uk',
       url='https://github.com/Pale-Blue-Dot-97/Minerva',
