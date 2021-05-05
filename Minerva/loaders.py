730ab7b261e7af9ed4cf3a7ef1df509c426439fe
Harry Baker <hjbaker97@gmail.com>
2021-05-05 10:27:25 +0100

    Completed TODO

--- Minerva/loaders.py
+++ Minerva/loaders.py
@@ -25,7 +25,7 @@ Institution: University of Southampton
 Created under a project funded by the Ordnance Survey Ltd
 
 TODO:
-    * Add IterableDataset for whole images
+    
 """
 # =====================================================================================================================
 #                                                     IMPORTS
