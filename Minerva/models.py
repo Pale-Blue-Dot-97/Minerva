42dfce38ff223711ac69ba5b78b6e3d7c74021a5
Harry Baker <hjbaker97@gmail.com>
2021-05-06 18:09:04 +0100

    Documented MLP

--- Minerva/models.py
+++ Minerva/models.py
@@ -17,7 +17,9 @@
     see <https://www.gnu.org/licenses/>.
 
 Author: Harry James Baker
+
 Email: hjb1d20@soton.ac.uk or hjbaker97@gmail.com
+
 Institution: University of Southampton
 
 Created under a project funded by the Ordnance Survey Ltd
@@ -38,71 +40,167 @@ from collections import OrderedDict
 #                                                     CLASSES
 # =====================================================================================================================
 class MLP(torch.nn.Module, ABC):
-    """
-    Simple class to construct a Multi-Layer Perceptron (MLP)
+    """Simple class to construct a Multi-Layer Perceptron (MLP).
+
+    Inherits from torch.nn.Module. Designed for use with PyTorch functionality.
+
+    Should be used in tandem with Trainer.
+
+    Attributes:
+        input_size (int): Size of the input vector to the network.
+        output_size (int): Size of the output vector of the network.
+        hidden_sizes (tuple[int] or list[int]): Size of the hidden layers within the network.
+            Can be a tuple[int] or list[int] of values that will also determine the number of layers other than
+            the required input and output layers.
+        network (torch.nn.Sequential):
+        criterion: PyTorch loss function model will use.
+        optimiser: PyTorch optimiser model will use. Initialised as None. Must be set using set_optimiser.
     """
 
-    def __init__(self, criterion, input_size=288, n_classes=8, hidden_sizes=(256, 144)):
+    def __init__(self, criterion, input_size: int = 288, n_classes: int = 8, hidden_sizes: tuple = (256, 144)):
+        """Initialises instance of MLP model.
+
+        Args:
+            criterion: PyTorch loss function model will use.
+            input_size (int): Optional; Size of the input vector to the network.
+            n_classes (int): Optional; Number of classes in input data.
+                Determines the size of the output vector of the network.
+            hidden_sizes (tuple[int] or list[int]): Optional; Size of the hidden layers within the network.
+                Can be a tuple[int] or list[int] of values that will also determine the number of layers other than
+                the required input and output layers.
+        """
         super(MLP, self).__init__()
+
         self.input_size = input_size
         self.output_size = n_classes
         self.hidden_sizes = hidden_sizes
-        self.layers = OrderedDict()
+        self._layers = OrderedDict()
 
+        # Constructs layers of the network based on the input size, the hidden sizes and the number of classes.
         for i in range(len(hidden_sizes)):
             if i is 0:
-                self.layers['Linear-0'] = torch.nn.Linear(input_size, hidden_sizes[i])
+                self._layers['Linear-0'] = torch.nn.Linear(input_size, hidden_sizes[i])
             elif i > 0:
-                self.layers['Linear-{}'.format(i)] = torch.nn.Linear(hidden_sizes[i - 1], hidden_sizes[i])
+                self._layers['Linear-{}'.format(i)] = torch.nn.Linear(hidden_sizes[i - 1], hidden_sizes[i])
             else:
                 print('EXCEPTION on Layer {}'.format(i))
-            self.layers['ReLu-{}'.format(i)] = torch.nn.ReLU()
 
-        self.layers['Classification'] = torch.nn.Linear(hidden_sizes[-1], n_classes)
+            # Adds ReLu activation after every linear layer
+            self._layers['ReLu-{}'.format(i)] = torch.nn.ReLU()
 
+        # Adds the final classification layer
+        self._layers['Classification'] = torch.nn.Linear(hidden_sizes[-1], n_classes)
+
+        # Constructs network from the OrderedDict of layers
         self.network = torch.nn.Sequential(self.layers)
 
+        # Sets loss function
         self.criterion = criterion
+
+        # Optimiser initialised as None as the model parameters created by its init is required to init a
+        # torch optimiser. The optimiser MUST be set by calling set_optimiser before the model can be trained.
         self.optimiser = None
 
     def set_optimiser(self, optimiser):
-        self.optimiser = optimiser
+        """Sets the optimiser used by the model.
 
-    def forward(self, x):
-        """Performs a forward pass of the network
+        Must be called after initialising a model and supplied with a PyTorch optimiser using this model's parameters.
 
         Args:
-            x (torch.Tensor): Data
+            optimiser: PyTorch optimiser model will use, initialised with this model's parameters.
+        """
+        self.optimiser = optimiser
+
+    def forward(self, x: torch.FloatTensor):
+        """Performs a forward pass of the network.
+
+        Can be called directly as a method of MLP (e.g. model.forward()) or when data is parsed to MLP (e.g. model()).
+
+        Args:
+            x (torch.FloatTensor): Input data to network.
 
         Returns:
-            y (torch.Tensor): Label
+            torch.Tensor of the likelihoods the network places on the input 'x' being of each class.
         """
         return self.network(x)
 
-    def step(self, x, y, train):
+    def step(self, x, y, train: bool):
+        """Generic step of model fitting using a batch of data.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+            train (bool): Sets whether this shall be a training step or not. True for training step which will then
+                clear the optimiser, and perform a backward pass of the network then update the optimiser.
+                If False for a validation or testing step, these actions are not taken.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
+        # Resets the optimiser's gradients if this is a training step.
         if train:
             self.optimiser.zero_grad()
 
-        # Forward pass
+        # Forward pass.
         z = self.forward(x)
 
-        # Compute Loss
+        # Compute Loss.
         loss = self.criterion(z, y)
 
+        # Performs a backward pass if this is a training step.
         if train:
-            # Backward pass
             loss.backward()
             self.optimiser.step()
 
         return loss, z
 
     def training_step(self, x, y):
+        """Calls step with train=True to perform a training step. See step for more details.
+
+        Designed to be compatible with Trainer and future compatibility with PyTorchLightning.
+        Hence the resulting `boilerplate' of this method and validation_step and testing_step.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
         return self.step(x, y, True)
 
     def validation_step(self, x, y):
+        """Calls step with train=False to perform a validation step. See step for more details.
+
+        Designed to be compatible with Trainer and future compatibility with PyTorchLightning.
+        Hence the resulting `boilerplate' of this method and training_step and testing_step.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
         return self.step(x, y, False)
 
     def testing_step(self, x, y):
+        """Calls step with train=False to perform a testing step. See step for more details.
+
+        Designed to be compatible with Trainer and future compatibility with PyTorchLightning.
+        Hence the resulting `boilerplate' of this method and validation_step and training_step.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
         return self.step(x, y, False)
 
 
