4ed14ede124e8da5d09c5aae8cd2e9d523f451c3
Harry Baker <hjbaker97@gmail.com>
2021-05-07 17:55:31 +0100

    Completed TODO

--- Minerva/utils/utils.py
+++ Minerva/utils/utils.py
@@ -26,7 +26,6 @@ Institution: University of Southampton
 Created under a project funded by the Ordnance Survey Ltd
 
 TODO:
-    * Fully document
 
 """
 # =====================================================================================================================
