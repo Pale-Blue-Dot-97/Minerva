b846ffd58df8ea7e7c938505497341207f9eba35
Harry Baker <hjbaker97@gmail.com>
2021-06-30 10:19:55 +0100

    Added ResNet to models.py

--- Minerva/models.py
+++ Minerva/models.py
@@ -31,6 +31,7 @@ TODO:
 #                                                     IMPORTS
 # =====================================================================================================================
 import torch
+from torchvision.models.resnet import BasicBlock, Bottleneck, conv1x1
 from abc import ABC
 import numpy as np
 from collections import OrderedDict
@@ -342,6 +343,217 @@ class CNN(torch.nn.Module, ABC):
 
         # Output from convolutional network is flattened and input to the fully connected network for classification.
         return self.fc_net(conv_out.view(-1, self.flattened_size))
+
+    def step(self, x, y, train: bool):
+        """Generic step of model fitting using a batch of data.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+            train (bool): Sets whether this shall be a training step or not. True for training step which will then
+                clear the optimiser, and perform a backward pass of the network then update the optimiser.
+                If False for a validation or testing step, these actions are not taken.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
+        # Resets the optimiser's gradients if this is a training step.
+        if train:
+            self.optimiser.zero_grad()
+
+        # Forward pass.
+        z = self.forward(x)
+
+        # Compute Loss.
+        loss = self.criterion(z, y)
+
+        # Performs a backward pass if this is a training step.
+        if train:
+            loss.backward()
+            self.optimiser.step()
+
+        return loss, z
+
+    def training_step(self, x, y):
+        """Calls step with train=True to perform a training step. See step for more details.
+
+        Designed to be compatible with Trainer and future compatibility with PyTorchLightning.
+        Hence the resulting `boilerplate' of this method and validation_step and testing_step.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
+        return self.step(x, y, True)
+
+    def validation_step(self, x, y):
+        """Calls step with train=False to perform a validation step. See step for more details.
+
+        Designed to be compatible with Trainer and future compatibility with PyTorchLightning.
+        Hence the resulting `boilerplate' of this method and training_step and testing_step.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
+        return self.step(x, y, False)
+
+    def testing_step(self, x, y):
+        """Calls step with train=False to perform a testing step. See step for more details.
+
+        Designed to be compatible with Trainer and future compatibility with PyTorchLightning.
+        Hence the resulting `boilerplate' of this method and validation_step and training_step.
+
+        Args:
+            x (torch.FloatTensor): Batch of input data to network.
+            y (torch.LongTensor): Batch of ground truth labels for the input data.
+
+        Returns:
+            loss: Loss computed by the loss function.
+            z: Predicted label for the input data by the network.
+        """
+        return self.step(x, y, False)
+
+
+class ResNet(torch.nn.Module, ABC):
+    """Modified version of the ResNet18 network to handle multi-spectral inputs and cross-entropy."""
+
+    def __init__(self, block, layers, in_channels: int = 3, num_classes: int = 1000, zero_init_residual: bool = False,
+                 groups: int = 1, width_per_group: int = 64, replace_stride_with_dilation=None,
+                 norm_layer=None) -> None:
+        super(ResNet, self).__init__()
+        if norm_layer is None:
+            norm_layer = torch.nn.BatchNorm2d
+        self._norm_layer = norm_layer
+
+        self.inplanes = 64
+        self.dilation = 1
+        if replace_stride_with_dilation is None:
+            # each element in the tuple indicates if we should replace
+            # the 2x2 stride with a dilated convolution instead
+            replace_stride_with_dilation = [False, False, False]
+        if len(replace_stride_with_dilation) != 3:
+            raise ValueError("replace_stride_with_dilation should be None "
+                             "or a 3-element tuple, got {}".format(replace_stride_with_dilation))
+        self.groups = groups
+        self.base_width = width_per_group
+        self.conv1 = torch.nn.Conv2d(in_channels, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = norm_layer(self.inplanes)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2,
+                                       dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2,
+                                       dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2,
+                                       dilate=replace_stride_with_dilation[2])
+        self.avgpool = torch.nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = torch.nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (torch.nn.BatchNorm2d, torch.nn.GroupNorm)):
+                torch.nn.init.constant_(m.weight, 1)
+                torch.nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
+                elif isinstance(m, BasicBlock):
+                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
+
+    def _make_layer(self, block, planes: int, blocks: int,
+                    stride: int = 1, dilate: bool = False) -> torch.nn.Sequential:
+        norm_layer = self._norm_layer
+        downsample = None
+        previous_dilation = self.dilation
+        if dilate:
+            self.dilation *= stride
+            stride = 1
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = torch.nn.Sequential(
+                conv1x1(self.inplanes, planes * block.expansion, stride),
+                norm_layer(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
+                            self.base_width, previous_dilation, norm_layer))
+        self.inplanes = planes * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(self.inplanes, planes, groups=self.groups,
+                                base_width=self.base_width, dilation=self.dilation,
+                                norm_layer=norm_layer))
+
+        return torch.nn.Sequential(*layers)
+
+    def _forward_impl(self, x: torch.Tensor) -> torch.Tensor:
+        # See note [TorchScript super()]
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+
+        return x
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self._forward_impl(x)
+
+
+class ResNet18(ResNet):
+
+    def __init__(self, criterion, input_shape=(12, 256, 256), n_classes: int = 8, zero_init_residual: bool = False,
+                 groups: int = 1, width_per_group: int = 64, replace_stride_with_dilation=None,
+                 norm_layer=None):
+        super(ResNet18, self).__init__(BasicBlock, [2, 2, 2, 2], in_channels=input_shape[0], num_classes=n_classes,
+                                       zero_init_residual=zero_init_residual, groups=groups,
+                                       width_per_group=width_per_group,
+                                       replace_stride_with_dilation=replace_stride_with_dilation,
+                                       norm_layer=norm_layer)
+
+        self.input_shape = input_shape
+        self.n_classes = n_classes
+
+        # Set the loss function.
+        self.criterion = criterion
+
+        # Initialises the optimiser as None. MUST be set after init of CNN to a torch optimiser with this model's
+        # parameters using set_optimiser before training can proceed.
+        self.optimiser = None
+
+    def set_optimiser(self, optimiser):
+        """Sets the optimiser used by the model.
+
+        Must be called after initialising a model and supplied with a PyTorch optimiser using this model's parameters.
+
+        Args:
+            optimiser: PyTorch optimiser model will use, initialised with this model's parameters.
+        """
+        self.optimiser = optimiser
 
     def step(self, x, y, train: bool):
         """Generic step of model fitting using a batch of data.
