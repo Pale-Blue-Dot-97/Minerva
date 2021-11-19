91c1ce635348141f4678b7a40993835400311f75
Harry Baker <hjbaker97@gmail.com>
2021-11-19 17:34:07 +0000

    Completed TODO

--- Minerva/models.py
+++ Minerva/models.py
@@ -25,7 +25,6 @@ Institution: University of Southampton
 Created under a project funded by the Ordnance Survey Ltd.
 
 TODO:
-    * Add missing docstrings
 """
 # =====================================================================================================================
 #                                                     IMPORTS
