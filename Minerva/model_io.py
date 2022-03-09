528f1692fc2179cc60b7859acc5918a95cc3a04b
Harry <hjbaker97@gmail.com>
2022-03-09 17:55:15 +0000

    Added `model_io` module

---
+++ Minerva/model_io.py
@@ -0,0 +1,36 @@
+"""Module to handle various IO from `dataloaders` and to models.
+
+    Copyright (C) 2022 Harry James Baker
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program in LICENSE.txt. If not,
+    see <https://www.gnu.org/licenses/>.
+
+Author: Harry James Baker
+
+Email: hjb1d20@soton.ac.uk or hjbaker97@gmail.com
+
+Institution: University of Southampton
+
+Created under a project funded by the Ordnance Survey Ltd.
+
+TODO:
+"""
+# =====================================================================================================================
+#                                                     IMPORTS
+# =====================================================================================================================
+
+
+# =====================================================================================================================
+#                                                     METHODS
+# =====================================================================================================================
