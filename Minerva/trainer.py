ffcb59a5b88b7dcbec64484f843d08d53bbea477
Harry <hjbaker97@gmail.com>
2022-03-22 15:21:56 +0000

    Bug fixing

--- Minerva/trainer.py
+++ Minerva/trainer.py
@@ -230,7 +230,7 @@ class Trainer:
                 bar()
 
         # Updates metrics with epoch results.
-        self.metric_logger.calc_metrics(mode, epoch_logger.get_logs(), self.params)
+        self.metric_logger.calc_metrics(mode, epoch_logger.get_logs(), **self.params)
 
         if self.params['calc_norm']:
             _ = utils.calc_grad(self.model)
