3884e5e8b9c23a2af03d1750191af13c589526e1
Harry <hjbaker97@gmail.com>
2022-03-22 11:59:01 +0000

    Fixed bug with `step_num`

--- Minerva/logger.py
+++ Minerva/logger.py
@@ -30,6 +30,8 @@ TODO:
 #                                                     IMPORTS
 # =====================================================================================================================
 from typing import Dict, Any
+from torch import Tensor
+from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import torch
 
@@ -38,19 +40,22 @@ import torch
 #                                                     CLASSES
 # =====================================================================================================================
 class STGLogger():
-    def __init__(self, n_batches: int, batch_size: int, out_shape, n_classes: int, record_int: bool = True, 
-                 record_float: bool = False) -> None:
+    def __init__(self, n_batches: int, batch_size: int, out_shape, n_classes: int, n_samples: int, 
+                 record_int: bool = True, record_float: bool = False) -> None:
         self.record_int = record_int
         self.record_float = record_float
-        
-        self.logs = {'batch_num' : 0,
-                     'total_loss' : 0.0,
-                     'total_correct' : 0.0,
-                     'labels': None,
-                     'predictions': None,
-                     'probs': None,
-                     'ids': [],
-                     'bounds': None}
+        self.batch_size = batch_size
+        self.n_classes = n_classes
+        self.n_samples = n_samples
+
+        self.logs: Dict[str, Any] = {'batch_num' : 0,
+                                     'total_loss' : 0.0,
+                                     'total_correct' : 0.0,
+                                     'labels': None,
+                                     'predictions': None,
+                                     'probs': None,
+                                     'ids': [],
+                                     'bounds': None}
 
         if self.record_int:
             self.logs['labels'] = np.empty((n_batches, batch_size, *out_shape), dtype=np.uint8)
@@ -67,7 +72,7 @@ class STGLogger():
             except MemoryError:
                 print('Dataset too large to record bounding boxes of samples!')
 
-    def log(self, loss, z, y, bbox, mode: str, step_num, writer) -> None:
+    def log(self, loss, z: Tensor, y: Tensor, bbox, mode: str, step_num: int, writer: SummaryWriter) -> None:
         if self.record_int:
             # Arg max the estimated probabilities and add to predictions.
             self.logs['predictions'][self.logs['batch_num']] = torch.argmax(z, 1)
@@ -90,9 +95,8 @@ class STGLogger():
         self.logs['total_loss'] += ls
         self.logs['total_correct'] += correct
 
-        writer.add_scalar(tag=f'{mode}_loss', scalar_value=ls, global_step=step_num[mode])
-        writer.add_scalar(tag=f'{mode}_acc', scalar_value=correct / len(torch.flatten(y)),
-                          global_step=step_num[mode])
+        writer.add_scalar(tag=f'{mode}_loss', scalar_value=ls, global_step=step_num)
+        writer.add_scalar(tag=f'{mode}_acc', scalar_value=correct / len(torch.flatten(y)), global_step=step_num)
 
     def get_logs(self) -> Dict[str, Any]:
         return self.logs
