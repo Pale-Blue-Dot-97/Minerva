10be7d0b1809c7adc6d8d03e3282324197a00af8
Harry <hjbaker97@gmail.com>
2022-03-24 16:16:28 +0000

    Abstract `MinervaLogger` and `SSL_Logger`

--- Minerva/logger.py
+++ Minerva/logger.py
@@ -34,60 +34,90 @@ from torch import Tensor
 from torch.utils.tensorboard import SummaryWriter
 import numpy as np
 import torch
+import abc
+from abc import ABC
 
 
 # =====================================================================================================================
 #                                                     CLASSES
 # =====================================================================================================================
-class STGLogger():
-    def __init__(self, n_batches: int, batch_size: int, out_shape, n_classes: int, n_samples: int, 
-                 record_int: bool = True, record_float: bool = False) -> None:
+class MinervaLogger(ABC):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, n_batches: int, batch_size: int, n_samples: int, record_int: bool = True, 
+                 record_float: bool = False) -> None:
+        super(MinervaLogger, self).__init__()
         self.record_int = record_int
         self.record_float = record_float
+        self.n_batches = n_batches
         self.batch_size = batch_size
-        self.n_classes = n_classes
         self.n_samples = n_samples
 
-        self.logs: Dict[str, Any] = {'batch_num' : 0,
-                                     'total_loss' : 0.0,
-                                     'total_correct' : 0.0,
-                                     'labels': None,
-                                     'predictions': None,
-                                     'probs': None,
-                                     'ids': [],
-                                     'bounds': None}
+        self.logs = {}
+        self.results = {}
+
+    @abc.abstractmethod
+    def log(self, mode: str, step_num: int, writer: SummaryWriter, loss, *args) -> None:
+        pass
+
+    @property
+    def get_logs(self) -> Dict[str, Any]:
+        return self.logs
+
+    @property
+    def get_results(self) -> Dict[str,Any]:
+        return self.results
+
+
+class STG_Logger(MinervaLogger):
+    def __init__(self, n_batches: int, batch_size: int, n_samples: int, out_shape, n_classes: int, 
+                 record_int: bool = True, record_float: bool = False) -> None:
+        super(STG_Logger, self).__init__(n_batches, batch_size, n_samples, record_int, record_float)
+
+        self.logs: Dict[str, Any] = {
+            'batch_num' : 0,
+            'total_loss' : 0.0,
+            'total_correct' : 0.0}
+
+        self.results: Dict[str, Any] = {
+            'y': None,
+            'z': None,
+            'probs': None,
+            'ids': [],
+            'bounds': None}
 
         if self.record_int:
-            self.logs['labels'] = np.empty((n_batches, batch_size, *out_shape), dtype=np.uint8)
-            self.logs['predictions'] = np.empty((n_batches, batch_size, *out_shape), dtype=np.uint8)
+            self.results['y'] = np.empty((self.n_batches, self.batch_size, *out_shape), dtype=np.uint8)
+            self.results['z'] = np.empty((self.n_batches, self.batch_size, *out_shape), dtype=np.uint8)
 
         if self.record_float:
             try:
-                self.logs['probs'] = np.empty((n_batches, batch_size, n_classes, *out_shape), dtype=np.float16)
+                self.results['probs'] = np.empty((self.n_batches, self.batch_size, n_classes, *out_shape), 
+                                              dtype=np.float16)
             except MemoryError:
                 print('Dataset too large to record probabilities of predicted classes!')
 
             try:
-                self.logs['bounds'] = np.empty((n_batches, batch_size), dtype=object)
+                self.results['bounds'] = np.empty((self.n_batches, self.batch_size), dtype=object)
             except MemoryError:
                 print('Dataset too large to record bounding boxes of samples!')
 
-    def log(self, loss, z: Tensor, y: Tensor, bbox, mode: str, step_num: int, writer: SummaryWriter) -> None:
+    def log(self, mode: str, step_num: int, writer: SummaryWriter, loss, z: Tensor, y: Tensor, bbox) -> None:
         if self.record_int:
             # Arg max the estimated probabilities and add to predictions.
-            self.logs['predictions'][self.logs['batch_num']] = torch.argmax(z, 1)
+            self.results['z'][self.logs['batch_num']] = torch.argmax(z, 1).cpu().numpy()
 
             # Add the labels and sample IDs to lists.
-            self.logs['labels'][self.logs['batch_num']] = y.numpy()
+            self.results['y'][self.logs['batch_num']] = y.cpu().numpy()
             batch_ids = []
             for i in range(self.logs['batch_num'] * self.batch_size, (self.logs['batch_num'] + 1) * self.batch_size):
                 batch_ids.append(str(i).zfill(len(str(self.n_samples))))
-            self.logs['ids'].append(batch_ids)
+            self.results['ids'].append(batch_ids)
 
         if self.record_float:
             # Add the estimated probabilities to probs.
-            self.logs['probs'][self.logs['batch_num']] = z.numpy()
-            self.logs['bounds'][self.logs['batch_num']] = bbox
+            self.results['probs'][self.logs['batch_num']] = z.detach().cpu().numpy()
+            self.results['bounds'][self.logs['batch_num']] = bbox
 
         ls = loss.item()
         correct = (torch.argmax(z, 1) == y).sum().item()
@@ -98,5 +128,21 @@ class STGLogger():
         writer.add_scalar(tag=f'{mode}_loss', scalar_value=ls, global_step=step_num)
         writer.add_scalar(tag=f'{mode}_acc', scalar_value=correct / len(torch.flatten(y)), global_step=step_num)
 
-    def get_logs(self) -> Dict[str, Any]:
-        return self.logs
+        self.logs['batch_num'] += 1
+
+
+class SSL_Logger(MinervaLogger):
+    def __init__(self, n_batches: int, batch_size: int, n_samples: int, out_shape=None, n_classes: int = None, 
+                 record_int: bool = True, record_float: bool = False) -> None:
+        super(SSL_Logger, self).__init__(n_batches, batch_size, n_samples, record_int, record_float)
+
+        self.logs: Dict[str, Any] = {'batch_num' : 0,
+                                     'total_loss' : 0.0}
+
+    def log(self, mode: str, step_num: int, writer: SummaryWriter, loss, z: Tensor, y: Tensor, bbox) -> None:
+        ls = loss.item()
+        self.logs['total_loss'] += ls
+
+        writer.add_scalar(tag=f'{mode}_loss', scalar_value=ls, global_step=step_num)
+
+        self.logs['batch_num'] += 1
