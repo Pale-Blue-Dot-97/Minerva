c19961186a5942d5b507cb117b46af994a11d69a
Harry <hjbaker97@gmail.com>
2022-03-29 18:48:09 +0100

    Moved `sphinx` files to `docs/`

--- docs/conf.py
+++ docs/conf.py
@@ -28,8 +28,9 @@ author = 'Harry Baker'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'myst_parser',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autodoc',
+    'myst_parser'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. Minerva documentation master file, created by
-   sphinx-quickstart on Tue Mar 29 17:50:17 2022.
+   sphinx-quickstart on Tue Mar 29 18:44:06 2022.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
