15704ea94c31f4edb98cd67a76c1cfa8789ea373
Harry <hjbaker97@gmail.com>
2022-04-28 22:40:58 +0100

    Added `MinervaPipe.py`

---
+++ minerva/bin/MinervaPipe.py
@@ -0,0 +1,76 @@
+"""Script to handle the pre-training of model and its subsequent downstream task fine-tuning.
+
+    Copyright (C) 2022 Harry James Baker
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program in LICENSE.txt. If not,
+    see <https://www.gnu.org/licenses/>.
+
+Author: Harry James Baker
+
+Email: hjb1d20@soton.ac.uk or hjbaker97@gmail.com
+
+Institution: University of Southampton
+
+Created under a project funded by the Ordnance Survey Ltd.
+
+TODO:
+"""
+# =====================================================================================================================
+#                                                     IMPORTS
+# =====================================================================================================================
+from typing import Dict, Any
+import os
+import sys
+import yaml
+import argparse
+
+
+# =====================================================================================================================
+#                                                      MAIN
+# =====================================================================================================================
+def main(config_path: str):
+
+    with open(config_path) as f:
+        config: Dict[str, Any] = yaml.safe_load(f)
+
+    for key in config.keys():
+        print(
+            f"\nExecuting {key} experiment + ====================================================================="
+        )
+
+        try:
+            exit_code = os.system(f"python MinervaExp.py -c {config[key]}")
+            if exit_code != 0:
+                raise SystemExit()
+        except KeyboardInterrupt as err:
+            print(f"{err}: Skipping to next experiment...")
+
+        except SystemExit as err:
+            print(err)
+            print(f"Error in {key} experiment -> ABORT")
+            sys.exit(exit_code)
+
+        print(
+            f"\n{key} experiment COMPLETE + ====================================================================="
+        )
+
+    print("\nPipeline COMPLETE")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config_path", type=str)
+    args = parser.parse_args()
+
+    main(config_path=args.config_path)
