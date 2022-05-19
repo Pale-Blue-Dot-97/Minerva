88eb524c5dae40c7551033ee12d4ff1fd6f40306
Harry <hjbaker97@gmail.com>
2022-05-19 19:10:08 +0100

    Added `PairDataset` class

---
+++ minerva/datasets.py
@@ -0,0 +1,61 @@
+"""Module containing custom samplers for `torch` datasets.
+
+    Copyright (C) 2022 Harry James Baker
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program in LICENSE.txt. If not,
+    see <https://www.gnu.org/licenses/>.
+
+Author: Harry James Baker
+
+Email: hjb1d20@soton.ac.uk or hjbaker97@gmail.com
+
+Institution: University of Southampton
+
+Created under a project funded by the Ordnance Survey Ltd.
+
+TODO:
+"""
+# =====================================================================================================================
+#                                                     IMPORTS
+# =====================================================================================================================
+from typing import Tuple, Optional, Callable, Dict, Any, Iterable
+from torchgeo.datasets import GeoDataset
+from torchgeo.datasets.utils import BoundingBox
+
+
+# =====================================================================================================================
+#                                                     CLASSES
+# =====================================================================================================================
+class PairedDataset(GeoDataset):
+    def __init__(
+        self,
+        dataset,
+        transforms: Optional[Callable[[Dict[str, Any]], Dict[str, Any]]] = None,
+    ) -> None:
+        super().__init__(transforms)
+        self.dataset = dataset
+
+    def __getitem__(self, queries: Iterable[BoundingBox]) -> Tuple[Dict[str, Any], ...]:
+        samples = []
+        for query in queries:
+            samples.append(self.dataset.__getitem__(query))
+        return tuple(samples)
+
+    def __getattr__(self, item):
+        if item in self.__dict__:
+            return getattr(self, item)
+        elif item in self.dataset.__dict__:
+            return getattr(self.dataset, item)
+        else:
+            raise AttributeError
