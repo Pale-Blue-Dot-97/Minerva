838b29a72550144a421092085edfa50e03510645
Harry <hjbaker97@gmail.com>
2022-06-01 00:33:45 +0100

    Added `test_models.py`

---
+++ tests/test_models.py
@@ -0,0 +1,31 @@
+from minerva.models import (
+    MLP,
+    CNN,
+    ResNet,
+    ResNet18,
+    ResNet34,
+    ResNet50,
+    ResNet101,
+    ResNet152,
+    FCNResNet18,
+    FCNResNet34,
+    FCNResNet50,
+    FCN32ResNet18,
+    FCN32ResNet34,
+    FCN32ResNet50,
+    FCN16ResNet18,
+    FCN16ResNet34,
+    FCN16ResNet50,
+    FCN8ResNet18,
+    FCN8ResNet34,
+    FCN8ResNet50,
+    FCN8ResNet101,
+    FCN8ResNet152,
+    SimCLR18,
+    SimCLR34,
+    SimCLR50,
+)
+
+
+def test_mlp() -> None:
+    pass
