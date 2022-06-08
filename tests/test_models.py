8dedc4ec749a7a6c9dc2dbf6d02fcb8c267a304b
Harry <hjbaker97@gmail.com>
2022-06-08 18:43:05 +0100

    Aded `test_simclr`

--- tests/test_models.py
+++ tests/test_models.py
@@ -2,6 +2,7 @@ import minerva.models as mm
 import torch
 import numpy as np
 import pytest
+from pytorch_metric_learning.losses import NTXentLoss
 
 
 criterion = torch.nn.CrossEntropyLoss()
@@ -35,7 +36,8 @@ def test_mlp() -> None:
 
 
 def test_cnn() -> None:
-    model = mm.CNN(criterion)
+    input_size = (4, 64, 64)
+    model = mm.CNN(criterion, input_size=input_size)
 
     optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
 
@@ -44,13 +46,13 @@ def test_cnn() -> None:
     model.determine_output_dim()
     assert model.output_shape is model.n_classes
 
-    x = torch.rand(16, *(4, 256, 256))
-    y = torch.LongTensor(np.random.randint(0, 8, size=16))
+    x = torch.rand(6, *input_size)
+    y = torch.LongTensor(np.random.randint(0, 8, size=6))
 
     loss, z = model.training_step(x, y)
 
     assert type(loss.item()) is float
-    assert z.size() == (16, 8)
+    assert z.size() == (6, 8)
 
 
 def test_resnets() -> None:
@@ -67,38 +69,42 @@ def test_resnets() -> None:
         loss, z = test_model.training_step(x, y)
 
         assert type(loss.item()) is float
-        assert z.size() == (16, 8)
+        assert z.size() == (6, 8)
 
     with pytest.raises(ValueError):
         _ = mm.ResNet18(replace_stride_with_dilation=(True, False))
 
-    x = torch.rand(16, *(4, 256, 256))
-    y = torch.LongTensor(np.random.randint(0, 8, size=16))
+    input_size = (4, 64, 64)
+
+    x = torch.rand(6, *input_size)
+    y = torch.LongTensor(np.random.randint(0, 8, size=6))
 
     for zero_init_residual in (True, False):
 
-        model = mm.ResNet18(criterion, zero_init_residual=zero_init_residual)
+        model = mm.ResNet18(
+            criterion, input_size=input_size, zero_init_residual=zero_init_residual
+        )
 
         resnet_test(model, x, y)
 
     for model in (
-        mm.ResNet34(criterion),
-        mm.ResNet50(criterion),
-        mm.ResNet101(criterion),
-        mm.ResNet152(criterion),
+        mm.ResNet34(criterion, input_size=input_size),
+        mm.ResNet50(criterion, input_size=input_size),
+        mm.ResNet101(criterion, input_size=input_size),
+        mm.ResNet152(criterion, input_size=input_size),
     ):
         resnet_test(model, x, y)
 
-    encoder = mm.ResNet18(criterion, encoder=True)
+    encoder = mm.ResNet18(criterion, input_size=input_size, encoder=True)
     optimiser = torch.optim.SGD(encoder.parameters(), lr=1.0e-3)
 
     encoder.set_optimiser(optimiser)
 
     encoder.determine_output_dim()
     print(encoder.output_shape)
-    assert encoder.output_shape == (512, 8, 8)
+    assert encoder.output_shape == (512, 2, 2)
 
-    x = torch.rand(16, *(4, 256, 256))
+    x = torch.rand(6, *input_size)
     assert len(encoder(x)) is 5
 
 
@@ -111,32 +117,59 @@ def test_fcnresnets() -> None:
         test_model.set_optimiser(optimiser)
 
         test_model.determine_output_dim()
-        assert test_model.output_shape == (256, 256)
+        assert test_model.output_shape == (64, 64)
 
         loss, z = test_model.training_step(x, y)
 
         assert type(loss.item()) is float
-        assert z.size() == (16, 8, 256, 256)
+        assert z.size() == (6, 8, 64, 64)
 
-    x = torch.rand((16, 4, 256, 256))
-    y = torch.randint(0, 8, (16, 256, 256))
+    input_size = (4, 64, 64)
+
+    x = torch.rand((6, *input_size))
+    y = torch.randint(0, 8, (6, 64, 64))
 
     for model in (
-        mm.FCN32ResNet18(criterion),
-        mm.FCN32ResNet34(criterion),
-        mm.FCN32ResNet50(criterion),
-        mm.FCN16ResNet18(criterion),
-        mm.FCN16ResNet34(criterion),
-        mm.FCN16ResNet50(criterion),
-        mm.FCN8ResNet18(criterion),
-        mm.FCN8ResNet18(criterion),
-        mm.FCN8ResNet34(criterion),
-        mm.FCN8ResNet50(criterion),
-        mm.FCN8ResNet101(criterion),
-        mm.FCN8ResNet152(criterion),
+        mm.FCN32ResNet18(criterion, input_size=input_size),
+        mm.FCN32ResNet34(criterion, input_size=input_size),
+        mm.FCN32ResNet50(criterion, input_size=input_size),
+        mm.FCN16ResNet18(criterion, input_size=input_size),
+        mm.FCN16ResNet34(criterion, input_size=input_size),
+        mm.FCN16ResNet50(criterion, input_size=input_size),
+        mm.FCN8ResNet18(criterion, input_size=input_size),
+        mm.FCN8ResNet18(criterion, input_size=input_size),
+        mm.FCN8ResNet34(criterion, input_size=input_size),
+        mm.FCN8ResNet50(criterion, input_size=input_size),
+        mm.FCN8ResNet101(criterion, input_size=input_size),
+        mm.FCN8ResNet152(criterion, input_size=input_size),
     ):
         resnet_test(model, x, y)
 
 
 def test_simclr() -> None:
-    pass
+    loss_func = NTXentLoss()
+
+    input_size = (4, 64, 64)
+
+    x = torch.rand((6, *input_size))
+    y = torch.arange(len(x))
+    y = torch.cat([y, y], dim=0)
+
+    x = torch.stack([x, x])
+
+    for model in (
+        mm.SimCLR18(loss_func, input_size=input_size),
+        mm.SimCLR34(loss_func, input_size=input_size),
+        mm.SimCLR50(loss_func, input_size=input_size),
+    ):
+        optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
+
+        model.set_optimiser(optimiser)
+
+        model.determine_output_dim(sample_pairs=True)
+        assert model.output_shape is 128
+
+        loss, z = model.training_step(x, y)
+
+        assert type(loss.item()) is float
+        assert z.size() == (12, 128)
