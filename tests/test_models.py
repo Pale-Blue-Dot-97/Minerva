135ab0421e55bad6bc53cc9cdab0a3547303cc77
Harry <hjbaker97@gmail.com>
2022-06-08 16:07:25 +0100

    Added `test_resnet34`

--- tests/test_models.py
+++ tests/test_models.py
@@ -46,24 +46,20 @@ def test_cnn() -> None:
     x = torch.rand(16, *(4, 256, 256))
     y = torch.LongTensor(np.random.randint(0, 8, size=16))
 
-    for mode in ("train", "val", "test"):
-        if mode == "train":
-            loss, z = model.training_step(x, y)
-        if mode == "val":
-            loss, z = model.validation_step(x, y)
-        if mode == "test":
-            loss, z = model.testing_step(x, y)
+    loss, z = model.training_step(x, y)
 
-        assert type(loss.item()) is float
-        assert z.size() == (16, 8)
+    assert type(loss.item()) is float
+    assert z.size() == (16, 8)
 
 
 def test_resnet18() -> None:
     with pytest.raises(ValueError):
         _ = mm.ResNet18(replace_stride_with_dilation=(True, False))
 
+    criterion = torch.nn.CrossEntropyLoss()
+
     for zero_init_residual in (True, False):
-        criterion = torch.nn.CrossEntropyLoss()
+
         model = mm.ResNet18(
             criterion, input_size=(4, 224, 224), zero_init_residual=zero_init_residual
         )
@@ -78,13 +74,28 @@ def test_resnet18() -> None:
         x = torch.rand(16, *(4, 224, 224))
         y = torch.LongTensor(np.random.randint(0, 8, size=16))
 
-        for mode in ("train", "val", "test"):
-            if mode == "train":
-                loss, z = model.training_step(x, y)
-            if mode == "val":
-                loss, z = model.validation_step(x, y)
-            if mode == "test":
-                loss, z = model.testing_step(x, y)
+        loss, z = model.training_step(x, y)
 
-            assert type(loss.item()) is float
-            assert z.size() == (16, 8)
+        assert type(loss.item()) is float
+        assert z.size() == (16, 8)
+
+
+def test_resnet34() -> None:
+    criterion = torch.nn.CrossEntropyLoss()
+
+    model = mm.ResNet34(criterion, input_size=(4, 224, 224))
+
+    optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
+
+    model.set_optimiser(optimiser)
+
+    model.determine_output_dim()
+    assert model.output_shape is model.n_classes
+
+    x = torch.rand(16, *(4, 224, 224))
+    y = torch.LongTensor(np.random.randint(0, 8, size=16))
+
+    loss, z = model.training_step(x, y)
+
+    assert type(loss.item()) is float
+    assert z.size() == (16, 8)
