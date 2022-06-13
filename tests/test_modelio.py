b547de1198ade806b7c0592ced7ee842b8eee47e
Harry <hjbaker97@gmail.com>
2022-06-13 17:18:39 +0100

    Added `test_ssl_pair_tg`

--- tests/test_modelio.py
+++ tests/test_modelio.py
@@ -1,20 +1,20 @@
 from minerva.modelio import ssl_pair_tg, sup_tg
-from minerva.models import FCN32ResNet18
+from minerva.models import FCN32ResNet18, SimCLR34
 import torch
 from torchgeo.datasets.utils import BoundingBox
+from simclr.modules import NT_Xent
 from numpy.testing import assert_array_equal
 
 
 input_size = (4, 224, 224)
-criterion = torch.nn.CrossEntropyLoss()
-model = FCN32ResNet18(criterion, input_size=input_size)
-optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
-model.set_optimiser(optimiser)
-
 device = torch.device("cpu")
 
 
 def test_sup_tg() -> None:
+    criterion = torch.nn.CrossEntropyLoss()
+    model = FCN32ResNet18(criterion, input_size=input_size)
+    optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
+    model.set_optimiser(optimiser)
 
     for mode in ("train", "val", "test"):
         images = torch.rand(size=(6, *input_size))
@@ -27,6 +27,7 @@ def test_sup_tg() -> None:
         }
 
         results = sup_tg(batch, model, device, mode)
+
         assert type(results[0]) is torch.Tensor
         assert results[1].size() == (6, 8, *input_size[1:])
         assert assert_array_equal(results[2], batch["mask"]) is None
@@ -34,4 +35,30 @@ def test_sup_tg() -> None:
 
 
 def test_ssl_pair_tg() -> None:
-    pass
+    criterion = NT_Xent(6, 0.5, 1)
+    model = SimCLR34(criterion, input_size=input_size)
+    optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
+    model.set_optimiser(optimiser)
+
+    for mode in ("train", "val", "test"):
+        images_1 = torch.rand(size=(6, *input_size))
+        bboxes_1 = [BoundingBox(0, 1, 0, 1, 0, 1)] * 6
+
+        batch_1 = {
+            "image": images_1,
+            "bbox": bboxes_1,
+        }
+
+        images_2 = torch.rand(size=(6, *input_size))
+        bboxes_2 = [BoundingBox(0, 1, 0, 1, 0, 1)] * 6
+
+        batch_2 = {
+            "image": images_2,
+            "bbox": bboxes_2,
+        }
+
+        results = ssl_pair_tg((batch_1, batch_2), model, device, mode)
+
+        assert type(results[0]) is torch.Tensor
+        assert results[1].size() == (12, 128)
+        assert results[2] == batch_1["bbox"] + batch_2["bbox"]
