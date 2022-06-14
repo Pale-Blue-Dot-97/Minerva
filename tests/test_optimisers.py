565f4b9e3fa1ae1c3f79eabd63d5b49cb9f867e1
Harry <hjbaker97@gmail.com>
2022-06-14 19:37:23 +0100

    Added `test_lars`

--- tests/test_optimisers.py
+++ tests/test_optimisers.py
@@ -1,5 +1,29 @@
 from minerva.optimisers import LARS
+from minerva.models import CNN
+import torch
+import pytest
 
 
 def test_lars() -> None:
-    pass
+    model = CNN(torch.nn.CrossEntropyLoss(), input_size=(3, 224, 224))
+
+    with pytest.raises(ValueError, match="Invalid learning rate: -0.1"):
+        optimiser = LARS(model.parameters(), lr=-0.1)
+
+    with pytest.raises(ValueError, match="Invalid momentum value: -0.1"):
+        optimiser = LARS(model.parameters(), lr=1.0, momentum=-0.1)
+
+    with pytest.raises(ValueError, match="Invalid weight_decay value: -0.01"):
+        optimiser = LARS(model.parameters(), lr=0.1, weight_decay=-0.01)
+
+    with pytest.raises(ValueError, match="Invalid LARS coefficient value: -0.02"):
+        optimiser = LARS(model.parameters(), lr=1.0, eta=-0.02)
+
+    model.set_optimiser(LARS(model.parameters(), lr=1.0e-3))
+
+    x = torch.rand(60, 6, 3, 224, 224)
+    y = torch.randint(0, 8, size=(60, 6))
+
+    for mode in (True, False):
+        for i in range(60):
+            loss, z = model.step(x[i], y[i], train=mode)
