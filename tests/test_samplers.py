69a14b67378faf3700e198cf6f8ebeb0d667963b
Harry <hjbaker97@gmail.com>
2022-07-17 21:33:06 +0100

    Partially added `test_randompairbatchgeosampler`

--- tests/test_samplers.py
+++ tests/test_samplers.py
@@ -31,17 +31,23 @@ def test_randompairgeosampler() -> None:
     assert len(batch[1]["image"]) == 8
 
 
-"""
 def test_randompairbatchgeosampler() -> None:
     dataset = PairedDataset(TestImgDataset, img_root, res=1.0)
 
-    sampler = RandomPairBatchGeoSampler(dataset, size=32, length=32, batch_size=8, max_r=52, tiles_per_batch=1)
-    loader = DataLoader(dataset, batch_size=8, sampler=sampler, collate_fn=stack_sample_pairs)
+    sampler = RandomPairBatchGeoSampler(
+        dataset, size=32, length=32, batch_size=8, max_r=52, tiles_per_batch=1
+    )
+    loader = DataLoader(
+        dataset, batch_size=8, sampler=sampler, collate_fn=stack_sample_pairs
+    )
 
-    batch = next(iter(loader))
+    assert isinstance(loader, DataLoader)
 
-    assert isinstance(batch[0], defaultdict)
-    assert isinstance(batch[1], defaultdict)
-    assert len(batch[0]["image"]) == 8
-    assert len(batch[1]["image"]) == 8
-"""
+    # TODO: Unknown bug in trying to yield from dataloader with batch sampler.
+
+    # batch = next(iter(loader))
+
+    # assert isinstance(batch[0], defaultdict)
+    # assert isinstance(batch[1], defaultdict)
+    # assert len(batch[0]["image"]) == 8
+    # assert len(batch[1]["image"]) == 8
