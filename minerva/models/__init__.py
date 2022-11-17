12c05e7ffc433c13147b2668a34d76af93eaf859
Harry <hjbaker97@gmail.com>
2022-11-17 19:50:20 +0000

    Import FCN models from `fcn.py`

---
+++ minerva/models/__init__.py
@@ -0,0 +1,13 @@
+from fcn import (
+    FCN8ResNet18,
+    FCN8ResNet34,
+    FCN8ResNet50,
+    FCN8ResNet101,
+    FCN8ResNet152,
+    FCN16ResNet18,
+    FCN16ResNet34,
+    FCN16ResNet50,
+    FCN32ResNet18,
+    FCN32ResNet34,
+    FCN32ResNet50,
+)
