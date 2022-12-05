35f5404b345842ca0f65a9951cf20d696446a931
Harry <hjbaker97@gmail.com>
2022-12-05 18:30:14 +0000

    More UNet debugging

--- minerva/models/unet.py
+++ minerva/models/unet.py
@@ -108,6 +108,10 @@ class Up(nn.Module):
             self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1: Tensor, x2: Tensor) -> Tensor:
+        # print("In forward:")
+        # print(f"{x1.size()=}")
+        # print(f"{x2.size()=}")
+
         x1 = self.up(x1)
         # input is CHW
         diffY = x2.size()[2] - x1.size()[2]
@@ -116,7 +120,6 @@ class Up(nn.Module):
         x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2, diffY // 2, diffY - diffY // 2])
 
         # if you have padding issues, see
-        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
         # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
         x = torch.cat([x2, x1], dim=1)
 
@@ -132,7 +135,68 @@ class OutConv(nn.Module):
         return self.conv(x)
 
 
-class UNet(MinervaModel, ABC):
+class UNet(MinervaModel):
+    def __init__(
+        self,
+        criterion: Any,
+        input_size: Tuple[int, ...] = (4, 256, 256),
+        n_classes: int = 8,
+        bilinear: bool = False,
+    ) -> None:
+        super(UNet, self).__init__(
+            criterion=criterion, input_shape=input_size, n_classes=n_classes
+        )
+
+        self.n_channels = input_size[0]
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(self.n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        print(f"{x.size()=}")
+
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        print(f"{x1.size()=}")
+        print(f"{x2.size()=}")
+        print(f"{x3.size()=}")
+        print(f"{x4.size()=}")
+        print(f"{x5.size()=}")
+
+        x = self.up1(x5, x4)
+        print(f"{x.size()=}")
+
+        x = self.up2(x, x3)
+        print(f"{x.size()=}")
+
+        x = self.up3(x, x2)
+        print(f"{x.size()=}")
+
+        x = self.up4(x, x1)
+        print(f"{x.size()=}")
+
+        logits = self.outc(x)
+        print(f"{logits.size()=}")
+
+        return logits
+
+
+class UNetR(MinervaModel, ABC):
     def __init__(
         self,
         criterion: Any,
@@ -144,7 +208,7 @@ class UNet(MinervaModel, ABC):
         freeze_backbone: bool = False,
         backbone_kwargs: Optional[Dict[str, Any]] = None,
     ) -> None:
-        super(UNet, self).__init__(
+        super(UNetR, self).__init__(
             criterion=criterion, input_shape=input_size, n_classes=n_classes
         )
 
@@ -171,40 +235,47 @@ class UNet(MinervaModel, ABC):
         assert isinstance(backbone_out_shape, Sequence)
 
         print(backbone_out_shape)
+        latent_channels = backbone_out_shape[0] * 2
 
-        self.up1 = Up(backbone_out_shape[0], 512 // factor, bilinear)
-        self.up2 = Up(512, 256 // factor, bilinear)
-        self.up3 = Up(256, 128 // factor, bilinear)
-        self.up4 = Up(128, 64, bilinear)
-        self.outc = OutConv(64, n_classes)
+        # Final extra conv down to latent space from backbone output.
+        self.down4 = Down(latent_channels // 2, latent_channels // factor)
+
+        # De-conv back up concating in skip connections from backbone.
+        self.up1 = Up(latent_channels, latent_channels // 2 * factor, bilinear)
+        self.up2 = Up(latent_channels // 2, latent_channels // 4 * factor, bilinear)
+        self.up3 = Up(latent_channels // 4, latent_channels // 8 * factor, bilinear)
+        self.up4 = Up(latent_channels // 8, latent_channels // 16, bilinear)
+        self.outc = OutConv(latent_channels // 16, n_classes)
 
     def forward(self, x: Tensor) -> Tensor:
-        x4, x3, x2, x1, x0 = self.backbone(x)
+        x4, x3, x2, x1, _ = self.backbone(x)
 
-        print(f"{x0.size()=}")
         print(f"{x1.size()=}")
         print(f"{x2.size()=}")
         print(f"{x3.size()=}")
         print(f"{x4.size()=}")
 
-        x = self.up1(x4, x3)
+        x5 = self.down4(x4)
+
+        x = self.up1(x5, x4)
         print(f"{x.size()=}")
 
-        x = self.up2(x, x2)
+        x = self.up2(x, x3)
         print(f"{x.size()=}")
 
-        x = self.up3(x, x1)
+        x = self.up3(x, x2)
         print(f"{x.size()=}")
 
-        x = self.up4(x, x0)
+        x = self.up4(x, x1)
         print(f"{x.size()=}")
 
         logits = self.outc(x)
+        print(f"{logits.size()=}")
 
         return logits
 
 
-class UNetR18(UNet):
+class UNetR18(UNetR):
     def __init__(
         self,
         criterion: Any,
