65269528b114812bdc468c4ccdd90081e291fce9
Harry Baker <harry.baker@os.uk>
2022-12-18 18:03:20 +0000

    Added `test_universal_path` and `test_check_paths`

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,4 +1,21 @@
-from minerva.utils.config_load import load_configs
+import os
+from pathlib import Path
+from minerva.utils.config_load import (
+    load_configs,
+    universal_path,
+    check_paths,
+    chdir_to_default,
+)
+
+
+def test_universal_path():
+    path1 = "one/two/three/file.txt"
+    path2 = ["one", "two", "three", "file.txt"]
+
+    correct = Path(path1)
+
+    assert universal_path(path1) == correct
+    assert universal_path(path2) == correct
 
 
 def test_config_path(config_root, config_here):
@@ -12,3 +29,32 @@ def test_config_path(config_root, config_here):
     base, aux = load_configs(config_here / "exp_mf_config.yml")
     assert base
     assert aux
+
+
+def test_check_paths(config_root: Path):
+    path, config_name, config_path = check_paths(
+        config_root, use_default_conf_dir=False
+    )
+
+    assert path == str(config_root)
+    assert str(config_name) == config_root.name
+    assert config_path == config_root.parent
+
+    # Store the current working directory (i.e where script is being run from).
+    cwd = os.getcwd()
+
+    exp_config = "example_GeoCLR_config.yml"
+    path2, config_name2, config_path2 = check_paths(
+        exp_config, use_default_conf_dir=True
+    )
+
+    assert path2 == exp_config
+    assert config_name2 == exp_config
+    assert config_path2 == None
+
+    # Change the working directory back to script location.
+    os.chdir(cwd)
+
+
+def test_chdir_to_default():
+    pass
