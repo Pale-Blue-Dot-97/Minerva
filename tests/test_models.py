87e7cf4d6dce6259b2fb4a0eb37a29c877c514dd
Harry Baker <harry.baker@os.uk>
2022-12-22 00:06:07 +0000

    Moved depreciated model tests out

--- tests/test_models.py
+++ tests/test_models.py
@@ -30,56 +30,8 @@ from minerva.models import (
     FCN32ResNet34,
     FCN32ResNet50,
 )
-from minerva.models.__depreciated import MLP, CNN
 
 criterion = nn.CrossEntropyLoss()
-
-
-def test_mlp() -> None:
-    model_1 = MLP(criterion)
-    model_2 = MLP(criterion, hidden_sizes=128)
-
-    for model in (model_1, model_2):
-        optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
-
-        model.set_optimiser(optimiser)
-
-        model.determine_output_dim()
-        assert model.output_shape is model.n_classes
-
-        x = torch.rand(16, (288))
-        y = torch.LongTensor(np.random.randint(0, 8, size=16))
-
-        for mode in ("train", "val", "test"):
-            if mode == "train":
-                loss, z = model.step(x, y, train=True)
-            else:
-                loss, z = model.step(x, y, train=False)
-
-            assert type(loss.item()) is float
-            assert isinstance(z, Tensor)
-            assert z.size() == (16, 8)
-
-
-def test_cnn() -> None:
-    input_size = (4, 64, 64)
-    model = CNN(criterion, input_size=input_size)
-
-    optimiser = torch.optim.SGD(model.parameters(), lr=1.0e-3)
-
-    model.set_optimiser(optimiser)
-
-    model.determine_output_dim()
-    assert model.output_shape is model.n_classes
-
-    x = torch.rand(6, *input_size)
-    y = torch.LongTensor(np.random.randint(0, 8, size=6))
-
-    loss, z = model.step(x, y, train=True)
-
-    assert type(loss.item()) is float
-    assert isinstance(z, Tensor)
-    assert z.size() == (6, 8)
 
 
 def test_resnets() -> None:
