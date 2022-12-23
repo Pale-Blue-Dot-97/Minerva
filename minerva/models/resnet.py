6c95bf72043ae73aab0520232663b8ab824612c9
Harry Baker <harry.baker@os.uk>
2022-12-23 20:18:32 +0000

    `_preload_weights` now accept `NoneType` `weights`

--- minerva/models/resnet.py
+++ minerva/models/resnet.py
@@ -808,10 +808,13 @@ class ResNet152(MinervaModel, ABC):
 # =====================================================================================================================
 def _preload_weights(
     resnet: ResNet,
-    weights: Union[WeightsEnum, Any],
+    weights: Optional[Union[WeightsEnum, Any]],
     input_shape: Tuple[int, int, int],
     encoder_on: bool,
 ) -> ResNet:
+
+    if not weights:
+        return resnet
 
     if isinstance(weights, WeightsEnum):
         weights = weights.get_state_dict(True)
