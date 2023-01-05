42757420d627586e7fec795e2bf1a16f753744f0
Harry Baker <harry.baker@os.uk>
2023-01-05 15:34:15 +0000

    Refactored `test_ssl_metrics`

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -20,6 +20,8 @@ def test_sp_metrics() -> None:
 
         return logs
 
+    n_epochs = 2
+
     n_batches: Dict[str, int] = {
         "train": 12,
         "val": 4,
@@ -35,10 +37,11 @@ def test_sp_metrics() -> None:
         SP_Metrics(n_batches, 16, (4, 224, 224), model_type="scene_classifier")
     )
 
-    for mode in n_batches.keys():
-        logs = [get_random_logs(), get_random_logs()]
+    epochs = [k + 1 for k in range(n_epochs)]
 
-        epochs = [j + 1 for j in range(len(logs))]
+    for mode in n_batches.keys():
+        logs = [get_random_logs() for i in range(n_epochs)]
+
         correct_loss_1 = {
             "x": epochs,
             "y": [log["total_loss"] / n_batches[mode] for log in logs],
@@ -90,99 +93,87 @@ def test_sp_metrics() -> None:
 
 
 def test_ssl_metrics() -> None:
+    def get_random_logs() -> Dict[str, float]:
+        logs = {
+            "total_loss": random.random(),
+            "total_correct": random.random(),
+            "total_top5": random.random(),
+        }
+
+        return logs
+
+    n_epochs = 2
+
+    epochs = [k + 1 for k in range(n_epochs)]
+
     n_batches: Dict[str, int] = {"train": 12, "val": 4}
-    metric_logger_1 = SSL_Metrics(
-        n_batches, 16, (4, 224, 224), model_type="segmentation"
+
+    metric_loggers: List[MinervaMetrics] = []
+    metric_loggers.append(
+        SSL_Metrics(n_batches, 16, (4, 224, 224), model_type="segmentation")
     )
-    metric_logger_2 = SSL_Metrics(
-        n_batches, 16, (4, 224, 224), model_type="scene_classifier"
+    metric_loggers.append(
+        SSL_Metrics(n_batches, 16, (4, 224, 224), model_type="scene_classifier")
     )
 
     for mode in n_batches.keys():
-        logs_1 = {
-            "total_loss": random.random(),
-            "total_correct": random.random(),
-            "total_top5": random.random(),
+        logs = [get_random_logs(), get_random_logs()]
+
+        correct_loss = {
+            "x": epochs,
+            "y": [log["total_loss"] / n_batches[mode] for log in logs],
         }
 
-        logs_2 = {
-            "total_loss": random.random(),
-            "total_correct": random.random(),
-            "total_top5": random.random(),
-        }
+        correct_acc = []
+        correct_acc.append(
+            {
+                "x": epochs,
+                "y": [
+                    log["total_correct"] / (n_batches[mode] * 16 * 224 * 224)
+                    for log in logs
+                ],
+            }
+        )
 
-        metric_logger_1(mode, logs_1)
-        metric_logger_1.log_epoch_number(mode, 0)
-        metric_logger_1.print_epoch_results(mode, 0)
+        correct_acc.append(
+            {
+                "x": epochs,
+                "y": [log["total_correct"] / (n_batches[mode] * 16) for log in logs],
+            }
+        )
 
-        metric_logger_1(mode, logs_2)
-        metric_logger_1.log_epoch_number(mode, 1)
-        metric_logger_1.print_epoch_results(mode, 1)
+        correct_top5 = []
+        correct_top5.append(
+            {
+                "x": epochs,
+                "y": [
+                    log["total_top5"] / (n_batches[mode] * 16 * 224 * 224)
+                    for log in logs
+                ],
+            }
+        )
 
-        metric_logger_2(mode, logs_1)
-        metric_logger_2.log_epoch_number(mode, 0)
-        metric_logger_2.print_epoch_results(mode, 0)
+        correct_top5.append(
+            {
+                "x": epochs,
+                "y": [log["total_top5"] / (n_batches[mode] * 16) for log in logs],
+            }
+        )
 
-        metric_logger_2(mode, logs_2)
-        metric_logger_2.log_epoch_number(mode, 1)
-        metric_logger_2.print_epoch_results(mode, 1)
+        for i, metric_logger in enumerate(metric_loggers):
+            for j in range(len(logs)):
+                metric_logger(mode, logs[j])
+                metric_logger.log_epoch_number(mode, j)
+                metric_logger.print_epoch_results(mode, j)
 
-        metrics_1 = metric_logger_1.get_metrics
-        metrics_2 = metric_logger_2.get_metrics
+            metrics = metric_logger.get_metrics
 
-        correct_loss_1 = {
-            "x": [1, 2],
-            "y": [
-                logs_1["total_loss"] / n_batches[mode],
-                logs_2["total_loss"] / n_batches[mode],
-            ],
-        }
-        correct_acc_1 = {
-            "x": [1, 2],
-            "y": [
-                logs_1["total_correct"] / (n_batches[mode] * 16 * 224 * 224),
-                logs_2["total_correct"] / (n_batches[mode] * 16 * 224 * 224),
-            ],
-        }
+            assert metrics[f"{mode}_loss"] == pytest.approx(correct_loss)
+            assert metrics[f"{mode}_acc"] == pytest.approx(correct_acc[i])
+            assert metrics[f"{mode}_top5_acc"] == pytest.approx(correct_top5[i])
 
-        correct_acc_2 = {
-            "x": [1, 2],
-            "y": [
-                logs_1["total_correct"] / (n_batches[mode] * 16),
-                logs_2["total_correct"] / (n_batches[mode] * 16),
-            ],
-        }
-        correct_top5_1 = {
-            "x": [1, 2],
-            "y": [
-                logs_1["total_top5"] / (n_batches[mode] * 16 * 224 * 224),
-                logs_2["total_top5"] / (n_batches[mode] * 16 * 224 * 224),
-            ],
-        }
+            sub_metrics = metric_logger.get_sub_metrics()
 
-        correct_top5_2 = {
-            "x": [1, 2],
-            "y": [
-                logs_1["total_top5"] / (n_batches[mode] * 16),
-                logs_2["total_top5"] / (n_batches[mode] * 16),
-            ],
-        }
-
-        assert metrics_1[f"{mode}_loss"] == pytest.approx(correct_loss_1)
-        assert metrics_1[f"{mode}_acc"] == pytest.approx(correct_acc_1)
-        assert metrics_1[f"{mode}_top5_acc"] == pytest.approx(correct_top5_1)
-
-        assert metrics_2[f"{mode}_loss"] == pytest.approx(correct_loss_1)
-        assert metrics_2[f"{mode}_acc"] == pytest.approx(correct_acc_2)
-        assert metrics_2[f"{mode}_top5_acc"] == pytest.approx(correct_top5_2)
-
-        sub_metrics_1 = metric_logger_1.get_sub_metrics()
-        sub_metrics_2 = metric_logger_2.get_sub_metrics()
-
-        assert sub_metrics_1[f"{mode}_loss"] == pytest.approx(correct_loss_1)
-        assert sub_metrics_1[f"{mode}_acc"] == pytest.approx(correct_acc_1)
-        assert sub_metrics_1[f"{mode}_top5_acc"] == pytest.approx(correct_top5_1)
-
-        assert sub_metrics_2[f"{mode}_loss"] == pytest.approx(correct_loss_1)
-        assert sub_metrics_2[f"{mode}_acc"] == pytest.approx(correct_acc_2)
-        assert sub_metrics_2[f"{mode}_top5_acc"] == pytest.approx(correct_top5_2)
+            assert sub_metrics[f"{mode}_loss"] == pytest.approx(correct_loss)
+            assert sub_metrics[f"{mode}_acc"] == pytest.approx(correct_acc[i])
+            assert sub_metrics[f"{mode}_top5_acc"] == pytest.approx(correct_top5[i])
