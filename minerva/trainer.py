517dc7c01c79f44f3b49c326e5f6cd772b2ba4a3
Navid <navid.rahimi@os.uk>
2023-01-10 15:11:47 +0000

    logger updated for metrics per GPU node

--- minerva/trainer.py
+++ minerva/trainer.py
@@ -452,13 +452,12 @@ class Trainer:
                     dist.all_reduce(loss.div_(dist.get_world_size()))  # type: ignore[attr-defined]
                     results = (loss, *results[1:])
 
-                epoch_logger.log(mode, self.step_num[mode], self.writer, *results)
-
-                self.step_num[mode] += 1
-
                 # Updates progress bar that batch has been processed.
                 if self.gpu == 0:
+                    epoch_logger.log(mode, self.step_num[mode], self.writer, *results)
                     bar()
+
+                self.step_num[mode] += 1
 
         # Updates metrics with epoch results.
         self.metric_logger(mode, epoch_logger.get_logs)
