26e6b71eb9a3b71f4a8f5fd574690b360c036327
Harry <hjbaker97@gmail.com>
2023-01-25 19:03:44 +0000

    Added docstring for `models`

--- minerva/models/__init__.py
+++ minerva/models/__init__.py
@@ -1,4 +1,25 @@
 # flake8: noqa: F401
+# -*- coding: utf-8 -*-
+# Copyright (C) 2023 Harry Baker
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in LICENSE.txt. If not,
+# see <https://www.gnu.org/licenses/>.
+#
+# @org: University of Southampton
+# Created under a project funded by the Ordnance Survey Ltd.
+""":mod:`models` contains several types of models designed to work within :mod:`minerva`."""
+
 from .__depreciated import CNN as CNN
 from .__depreciated import MLP as MLP
 from .core import MinervaBackbone as MinervaBackbone
