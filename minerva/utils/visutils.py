3d56c8fd315e23861133d844a5301bb1d35f8fa1
Harry <hjbaker97@gmail.com>
2023-02-15 13:47:56 +0000

    Added more `pragma: no cover`

--- minerva/utils/visutils.py
+++ minerva/utils/visutils.py
@@ -947,7 +947,7 @@ def make_confusion_matrix(
             cm.astype(np.float16) / cm.sum(axis=1)[:, np.newaxis], decimals=2
         )
 
-    except RuntimeWarning as err:
+    except RuntimeWarning as err:  # pragma: no cover
         print("\n", err)
         print("At least one class had no ground truth or no predicted labels!")
 
@@ -1179,7 +1179,7 @@ def plot_embedding(
     if show:
         plt.show()
     if save:
-        if filename is None:
+        if filename is None:  # pragma: no cover
             filename = "tsne_cluster_vis.png"
         os.makedirs(Path(filename).parent, exist_ok=True)
         plt.savefig(filename)
