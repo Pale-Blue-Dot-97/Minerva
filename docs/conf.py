82077dddb41bb98efc66f2c1d3a5b74283f8594f
Harry Baker <harry.baker@os.uk>
2023-03-02 18:28:41 +0000

    Added more links to external documentation

--- docs/conf.py
+++ docs/conf.py
@@ -38,8 +38,12 @@ extensions = [
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
+    "typing": ("https://typing.readthedocs.io/en/latest/", None),
     "torch": ("https://pytorch.org/docs/stable/", None),
     "torchgeo": ("https://torchgeo.readthedocs.io/en/stable/", None),
+    "torchvision": ("https://pytorch.org/vision/stable/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
 }
 
 # Add any paths that contain templates here, relative to this directory.
