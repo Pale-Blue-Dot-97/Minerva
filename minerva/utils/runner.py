a659b963da63044be54c0236eb50ce5de8a66942
Harry <hjbaker97@gmail.com>
2024-03-08 16:51:30 +0000

    Added `max-r` argument to CLI

--- minerva/utils/runner.py
+++ minerva/utils/runner.py
@@ -61,10 +61,10 @@ import requests
 import torch
 import torch.distributed as dist
 import torch.multiprocessing as mp
-import wandb
 from wandb.sdk.lib import RunDisabled
 from wandb.sdk.wandb_run import Run
 
+import wandb
 from minerva.utils import CONFIG, MASTER_PARSER, utils
 
 # =====================================================================================================================
@@ -107,11 +107,19 @@ GENERIC_PARSER.add_argument(
 )
 
 GENERIC_PARSER.add_argument(
-    "--max_epochs",
+    "--max-epochs",
     dest="max_epochs",
     type=int,
     default=100,
     help="Maximum number of training epochs.",
+)
+
+GENERIC_PARSER.add_argument(
+    "--max-r",
+    dest="max_r",
+    type=int,
+    default=32,
+    help="Maximum distance in pixels between samples in a pair.",
 )
 
 GENERIC_PARSER.add_argument(
